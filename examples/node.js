f8f843412ba1b006902bf3cb7662f7e1d9969450
cesine <cesine@yahoo.com>
2015-06-23 14:54:15 -0400

    refactoring example to show what part does what, and what are reserved var names and what are their own var names

--- examples/node.js
+++ examples/node.js
@@ -1,49 +1,90 @@
 #!/usr/local/bin/node
 
-var d3 = require('d3');
-var cloud = require('../');
-var jsdom = require("jsdom").jsdom;
+(function() {
+  'use strict';
 
-var fs = require('fs');
+  var d3 = require('d3');
+  var cloud = require('../');
+  var jsdom = require("jsdom").jsdom;
+  var fs = require('fs');
 
-var document = jsdom("<body></body>");
-var fill = d3.scale.category20();
+  // Hoist all vars 
+  var document,
+    myFewWords,
+    myCloud,
+    myColorFunction,
+    myDrawFunction;
 
-var words = ["Hello", "world", "normally", "you", "want", "more", "words", "than", "this"]
-    .map(function(d) {
-      return {text: d, size: 10 + Math.random() * 90};
+  document = jsdom("<body></body>");
+
+  // Short hand to build an array of word objects with random importance
+  myFewWords = "Hello world normally you want more words than this".split(" ")
+    .map(function(word) {
+      return {
+        text: word,
+        importance: 10 + Math.random() * 90
+      };
     });
 
-cloud().size([900, 900])
-    .words(words)
+  // Each word object actually looks like this
+  myFewWords.push({
+    text: "example",
+    importance: 100
+  });
+
+  myColorFunction = d3.scale.category20();
+
+  // Ask d3-cloud to make an cloud object for us
+  myCloud = cloud();
+
+  // Configure our cloud with d3 chaining
+  myCloud
+    .size([900, 900])
+    .words(myFewWords)
     .padding(5)
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .font("Impact")
-    .fontSize(function(d) { return d.size; })
-    .on("end", function(words){
-        draw(words, document.body);
+    .rotate(function() {
+      return ~~(Math.random() * 2) * 90;
     })
-    .start();
+    .font("Impact")
+    .fontSize(function(word) {
+      return word.importance;
+    })
+    .on("end", function(words) {
+      myDrawFunction(words, document.body);
+    });
 
-console.log('Open output.svg and/or output.html to see the rendered output\n', document.body.innerHTML);
-fs.writeFileSync('output.html', document.body.innerHTML);
-fs.writeFileSync('output.svg', document.body.innerHTML);
 
-function draw(words, node) {
-    d3.select(node).append("svg")
-        .attr("width", 900)
-        .attr("height", 900)
+  // Declare our own draw function which will be called on the "end" event 
+  myDrawFunction = function(words, element) {
+    var svg = d3.select(element).append("svg");
+    svg.attr("width", 900)
+      .attr("height", 900)
       .append("g")
-        .attr("transform", "translate(450,450)")
+      .attr("transform", "translate(450,450)")
       .selectAll("text")
-        .data(words)
+      .data(words)
       .enter().append("text")
-        .style("font-size", function(d) { return d.size + "px"; })
-        .style("font-family", "Impact")
-        .style("fill", function(d, i) { return fill(i); })
-        .attr("text-anchor", "middle")
-        .attr("transform", function(d) {
-          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-        })
-        .text(function(d) { return d.text; });
- }
+      .style("font-size", function(word) {
+        return word.importance + "px";
+      })
+      .style("font-family", "Impact")
+      .style("fill", function(word, i) {
+        return myColorFunction(i);
+      })
+      .attr("text-anchor", "middle")
+      .attr("transform", function(word) {
+        return "translate(" + [word.x, word.y] + ")rotate(" + word.rotate + ")";
+      })
+      .text(function(word) {
+        return word.text;
+      });
+  };
+
+  // Run the render when you're ready
+  myCloud.start();
+
+  // Output the .svg or .html results to file, or res.send(document.body.innerHTML) if you are in a server context
+  console.log('Open output.svg and/or output.html to see the rendered output\n', document.body.innerHTML);
+  fs.writeFileSync('output.html', document.body.innerHTML);
+  fs.writeFileSync('output.svg', document.body.innerHTML);
+})();
