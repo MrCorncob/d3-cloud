c77fad0a06d811d4efbd4764e5806e1bc30321b4
cesine <cesine@yahoo.com>
2015-06-24 12:51:36 -0400

    basing replicable cloud off of hoisted examples/node.js
    and filling in setup tests for making sure the word's render features dont change upon subsequent start()

--- test/replicable-layout-spec.js
+++ test/replicable-layout-spec.js
@@ -1,6 +1,6 @@
 (function() {
 
-  /* globals require, describe, it, expect, console */
+  /* globals require, describe, it, expect, console, d3, document */
   'use strict';
 
   try {
@@ -22,21 +22,139 @@
 
       describe('Redraw a new cloud', function() {
 
+        // Hoist all vars 
+        var myFewWords,
+          myRerenderableCloud,
+          myColorFunction,
+          myDrawFunction,
+          redrawNewCloudElement,
+          WIDTH = 900,
+          HEIGHT = 900;
+
+        redrawNewCloudElement = localdocument.createElement("div");
+        redrawNewCloudElement.setAttribute("id", "redraw-new-cloud");
+        localdocument.body.appendChild(redrawNewCloudElement);
+
+        // Short hand to build an array of word objects with random importance
+        myFewWords = "As a user I want to be able to remove words and see roughly the same cloud".split(" ")
+          .map(function(word) {
+            return {
+              text: word,
+              importance: 10 + Math.random() * 90
+            };
+          });
+
+        myColorFunction = locald3.scale.category20();
+
+        // Ask d3-cloud to make an cloud object for us
+        myRerenderableCloud = d3CloudLayout();
+
+        // Configure our cloud with d3 chaining
+        myRerenderableCloud
+          .size([WIDTH, HEIGHT])
+          .words(myFewWords)
+          .padding(5)
+          .rotate(function() {
+            return ~~(Math.random() * 2) * 90;
+          })
+          .font("Impact")
+          .fontSize(function(word) {
+            return word.importance;
+          })
+          .on("end", function(words) {
+            myDrawFunction(words, redrawNewCloudElement);
+          });
+
+
+        // Declare our own draw function which will be called on the "end" event 
+        myDrawFunction = function(words, element) {
+          var svg = locald3.select(element).append("svg");
+          svg.attr("width", WIDTH)
+            .attr("height", HEIGHT)
+            .append("g")
+            .attr("transform", "translate(450,450)")
+            .selectAll("text")
+            .data(words)
+            .enter().append("text")
+            .style("font-size", function(word) {
+              return word.importance + "px";
+            })
+            .style("font-family", "Impact")
+            .style("fill", function(word, i) {
+              return myColorFunction(i);
+            })
+            .attr("text-anchor", "middle")
+            .attr("transform", function(word) {
+              return "translate(" + [word.x, word.y] + ")rotate(" + word.rotate + ")";
+            })
+            .text(function(word) {
+              return word.text;
+            });
+        };
+
+        it('should have its own element', function() {
+          expect(redrawNewCloudElement).toBeDefined();
+          expect(redrawNewCloudElement.children).toBeDefined();
+        });
+
         it('should have word objects', function() {
-          expect(true).toBeTruthy();
+          expect(myRerenderableCloud.words().length).toEqual(17);
+          expect(myRerenderableCloud.words()[13].text).toEqual('roughly');
+          expect(myRerenderableCloud.words()[15].text).toEqual('same');
         });
 
         it('should add render attributes upon start', function() {
-          expect(true).toBeTruthy();
+          myRerenderableCloud.start();
+
+          var representativeWord = myRerenderableCloud.words()[15];
+          expect(representativeWord.rotate).toBeDefined();
+          expect(representativeWord.size).toBeDefined();
+          expect(representativeWord.padding).toBeDefined();
+          expect(representativeWord.width).toBeDefined();
+          expect(representativeWord.height).toBeDefined();
+          expect(representativeWord.xoff).toBeDefined();
+          expect(representativeWord.yoff).toBeDefined();
+          expect(representativeWord.x1).toBeDefined();
+          expect(representativeWord.y1).toBeDefined();
+          expect(representativeWord.x0).toBeDefined();
+          expect(representativeWord.y0).toBeDefined();
+          expect(representativeWord.hasText).toBeDefined();
+          expect(representativeWord.x).toBeDefined();
+          expect(representativeWord.y).toBeDefined();
         });
 
         it('should not change word objects render attributes', function() {
-          expect(true).toBeTruthy();
+          var wordBeforeRerender = JSON.parse(JSON.stringify(myRerenderableCloud.words()[15]));
+
+          var cleanedWords = myRerenderableCloud.words();
+          var removedWord = cleanedWords.splice(13, 1);
+          expect(removedWord[0].text).toEqual('roughly');
+
+          myRerenderableCloud.words(cleanedWords);
+          expect(myRerenderableCloud.words().length).toEqual(16);
+
+          myRerenderableCloud.start();
+          var wordAfterRender =  myRerenderableCloud.words()[14];
+
+          expect(wordBeforeRerender.rotate).toEqual(wordAfterRender.rotate);
+          expect(wordBeforeRerender.size).toEqual(wordAfterRender.size);
+          expect(wordBeforeRerender.padding).toEqual(wordAfterRender.padding);
+          expect(wordBeforeRerender.width).toEqual(wordAfterRender.width);
+          expect(wordBeforeRerender.height).toEqual(wordAfterRender.height);
+          expect(wordBeforeRerender.xoff).toEqual(wordAfterRender.xoff);
+          expect(wordBeforeRerender.yoff).toEqual(wordAfterRender.yoff);
+          expect(wordBeforeRerender.x1).toEqual(wordAfterRender.x1);
+          expect(wordBeforeRerender.y1).toEqual(wordAfterRender.y1);
+          expect(wordBeforeRerender.x0).toEqual(wordAfterRender.x0);
+          expect(wordBeforeRerender.y0).toEqual(wordAfterRender.y0);
+          expect(wordBeforeRerender.hasText).toEqual(wordAfterRender.hasText);
+          expect(wordBeforeRerender.x).toEqual(wordAfterRender.x);
+          expect(wordBeforeRerender.y).toEqual(wordAfterRender.y);
         });
 
       });
 
-      describe('Redraw an existing cloud', function() {
+      xdescribe('Redraw an existing cloud', function() {
 
         it('should not change word objects render attributes', function() {
           expect(true).toBeTruthy();
